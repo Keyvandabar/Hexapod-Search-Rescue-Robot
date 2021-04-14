5c8c4a0da7670689bef51acd78ce23a51cde0f0e
Keyvandabar <Keycban@gmail.com>
2021-04-14 15:02:46 -0600

    Update camera.py

--- RaspiCode/CameraFlask/camera.py
+++ RaspiCode/CameraFlask/camera.py
@@ -7,6 +7,22 @@ from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 import numpy as np
+'''
+/*
+**********************************************
+Title: Pi Camera Stream Flask
+Author: Eben Kouao
+Date: Oct 10, 2020
+Code version: *
+Availability: https://github.com/EbenKouao/pi-camera-stream-flask
+- The primary function of this code allowed me to get a good introduction to opening and 
+streaming a flask camera stream with little to no latency in the video stream. After countless
+attempts at trying different methods of streaming the video I found this github and source
+to provide the best quality vs latency ratio and decided to adopt it to the program.
+*** THE PRIMARY FUNCTION OF THIS CODE IS TO ONLY STREAM THE VIDEO TO A LOCAL HTML PAGE
+***************************************************
+*/
+'''
 
 class VideoCamera(object):
     def __init__(self, flip = False):
